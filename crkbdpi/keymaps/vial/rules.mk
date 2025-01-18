0784b766badbddf21c415f9c08254e1b25408520
AlexMaroiu <maroiualex@gmail.com>
2025-01-18 17:58:23 +0200

    add vial keymap

---
+++ crkbdpi/keymaps/vial/rules.mk
@@ -0,0 +1,12 @@
+VIA_ENABLE          = yes
+VIAL_ENABLE         = yes
+LTO_ENABLE          = no
+
+RGBLIGHT_ENABLE     = no
+RGB_MATRIX_ENABLE   = yes # Can't have RGBLIGHT and RGB_MATRIX at the same time.
+MOUSEKEY_ENABLE     = yes
+OLED_ENABLE         = yes
+EXTRAKEY_ENABLE     = yes
+COMBO_ENABLE        = yes
+
+QMK_SETTINGS        = no
