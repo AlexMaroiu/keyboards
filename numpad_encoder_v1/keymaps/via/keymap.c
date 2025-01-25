a19482a33aa384b693a381a05dfa1684c1ab817a
AlexMaroiu <maroiualex@gmail.com>
2025-01-25 19:09:06 +0200

    fix keymap for numpad

--- numpad_encoder_v1/keymaps/via/keymap.c
+++ numpad_encoder_v1/keymaps/via/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
         KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
         KC_P4,   KC_P5,   KC_P6,   KC_NO,
-        KC_P1,   KC_P2,   KC_P3,   KC_EQL,
+        KC_P1,   KC_P2,   KC_P3,   KC_NO,
         KC_P0,   KC_PDOT, KC_PENT, KC_NO
     ),
     [1] = LAYOUT_ortho_6x4(
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
         KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
         KC_P4,   KC_P5,   KC_P6,   KC_NO,
-        KC_P1,   KC_P2,   KC_P3,   KC_EQL,
+        KC_P1,   KC_P2,   KC_P3,   KC_NO,
         KC_P0,   KC_PDOT, KC_PENT, KC_NO
     ),
     [2] = LAYOUT_ortho_6x4(
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
         KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
         KC_P4,   KC_P5,   KC_P6,   KC_NO,
-        KC_P1,   KC_P2,   KC_P3,   KC_EQL,
+        KC_P1,   KC_P2,   KC_P3,   KC_NO,
         KC_P0,   KC_PDOT, KC_PENT, KC_NO
     ),
     [3] = LAYOUT_ortho_6x4(
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
         KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
         KC_P4,   KC_P5,   KC_P6,   KC_NO,
-        KC_P1,   KC_P2,   KC_P3,   KC_EQL,
+        KC_P1,   KC_P2,   KC_P3,   KC_NO,
         KC_P0,   KC_PDOT, KC_PENT, KC_NO
     ),
 };
