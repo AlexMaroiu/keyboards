c84a267e61748e196828b4b5d44e5bf03521173a
AlexMaroiu <maroiualex@gmail.com>
2026-02-10 18:27:56 +0200

    add more info for wired keyboards

--- web-docs/src/pages/Wired.jsx
+++ web-docs/src/pages/Wired.jsx
@@ -5,6 +5,10 @@ export default function Wired() {
       <ol>
         <li>Trebuie sa fi atent sa fie conectat cablul jack (TRRS) dintre tastaturi bine. Daca nu este conectat pana la capat, atunci se poate arde microcontrollerul si se strica tastatura</li>
         <li>Pentru a modifica configuratia tastaturi se poate folosii Vial (https://get.vial.today/) sau Via (https://usevia.app/)</li>
+        <li>Alternativ se poate folosii Via pentru configurarea tastaturii (https://usevia.app/)</li>
+        <li>Pentru a modifica software-ul de pe tastatura se foloseste QMK (https://docs.qmk.fm/newbs_getting_started), recomandat impreuna cu Vial (https://github.com/vial-kb/vial-qmk)</li>
+        <li>Pentru a intra in bootloader se foloseste Magic boot key (de obicei tasta din colt-ul exterior sus, tasta ESC de exemplu pentru Lily58, jumatatea stanga)</li>
+        <li>Pentru a intra in bootloader pentru Corne se folosesc tastele Q si P in functie de tastatura</li>
 
       </ol>
     </div>
